1fbc01c918aaa6f1d8afdf2efc3d5921c8c4a7cd
IvanicsSz <ivanics.szilard@gmail.com>
2016-05-19 21:02:26 +0200

    Update snakk.py
    addons

--- snakk.py
+++ snakk.py
@@ -10,3 +10,12 @@ if x== dims[1]-len(message)-1:
   horizontal=-1
 elif x==0:
   horizontal=1
+# gameoverhez
+screen.clear()
+message1='Game Over'
+message2='press space to play again'
+message3='press enter to quit'
+#screen.addstr(dims[0]/2,(dims[1]-len(message1))/2,message1
+#..........
+#https://www.youtube.com/user/IsharaComixVideos/videos?sort=dd&view=0&shelf_id=0
+# érdemes kitenni a változókat initialize -ba mint global
