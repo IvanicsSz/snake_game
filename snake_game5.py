31b5258450dd17d074fde6a93182842799b9b7a8
Ivanics Szilárd <ivanics.szilard@gmail.com>
2016-05-20 09:05:33 +0200

    Snake clear

--- snake_game5.py
+++ snake_game5.py
@@ -5,7 +5,7 @@ from curses import wrapper
 import random
 
 
-
+#paint the wall
 def wall(space,direction):
     global screen
     dims=screen.getmaxyx()
@@ -26,7 +26,7 @@ def wall(space,direction):
             screen.addstr(20,i,"B")
         for i in range(dims[0]-space,space,-1):
             screen.addstr(i,20,"B")
-
+#keyboard events
 def keyboard(key,move):
     if key==27:
         return 2
@@ -42,56 +42,60 @@ def keyboard(key,move):
     if key==KEY_RIGHT and move[1]!=-1:
         move[0],move[1]=0,1
         return move
-
+# random place for the objects
 def food(gety,getx):
     foody=random.randrange(2,gety-2,1)
     foodx=random.randrange(2,getx-2,1)
     return foody,foodx
-
-def game_over(sn,sb,sy,sx,dimy,dimx):
+# handel the game over event
+def game_over(sn,sb,sy,sx,dimy,dimx,score):
     global screen
     if sn in sb or sy==1 or sy==dimy or sx==1 or sx==dimx:
-        screen.addstr(int(dimy/2),int(dimx/2),"GAME OVER")
+        screen.clear()
+        msg1="GAME OVER"
+        msg2="Your score: "
+        screen.addstr(int(dimy/2),int((dimx-len(msg1))/2),msg1)
+        screen.addstr(int(dimy/2)-2,int((dimx-len(msg2))/2),msg2+str(score))
         screen.refresh()
         time.sleep(2)
         return  2
-
-
-def main(stdscr):
-    # init screen
+# initialize curses screen, colors
+def init():
+    global screen
     curses.noecho()             # Disable default printing of inputs
     curses.curs_set(0)          # Hiding cursor visibility (https://docs.python.org/2/library/curses.html#curses.curs_set)
-    global screen
     screen=curses.newwin(0,0,0,0)   #create a new window with terminal size
     screen.keypad(1)               # enable processing of functional keys by curses (ex. arrow keys)
     screen.nodelay(1)       #continous getch command
-    dim=screen.getmaxyx()   #max y,x of the screen
     curses.start_color()
     curses.init_pair(1,curses.COLOR_CYAN,curses.COLOR_BLACK)
     curses.init_pair(2,curses.COLOR_GREEN,curses.COLOR_GREEN)
     curses.init_pair(3,curses.COLOR_RED,curses.COLOR_RED)
     curses.init_pair(4,curses.COLOR_YELLOW,curses.COLOR_YELLOW)
-    color=1
+    curses.init_pair(5,curses.COLOR_WHITE,curses.COLOR_BLACK)
 
-    speed=0.1
-    speed_change=0.01
-    score=0
+
+def main(stdscr):
+    init()
+    #main variables
+    global screen
+    dim=screen.getmaxyx()
+    color,speed,speed_change,score,space=1,0.1,0.01,0,1
     foods=[2,2]
     food_ok=True
     powers=[2,2]
     power_ok=True
-    move=[0,1]              #move the snake y,x
+    move=[0,1]
     snake=[10,10]
     monster=[20,20]
-    space=1
-    a=1
     snake_body=[snake[:]]*10
     last=snake_body[-1][:]
-    slen=len(snake)       #snake initial position
-    key=-1                  # define key unknown key
-
+    slen=len(snake)
+    key=-1
+    a=1
     wall(space,"round")
 
+    #main loop for the game actions
     while a!=2:
         time.sleep(speed)
         if last not in snake_body:
@@ -101,10 +105,11 @@ def main(stdscr):
 
         key=screen.getch()
         keyboard(key,move)
+        #let's move the snake
         snake[0]+=move[0]
         snake[1]+=move[1]
-
-        #food_ok=factory(food_ok,snake_body)
+        screen.addstr(snake[0],snake[1],"X",curses.color_pair(5)|curses.A_BOLD)
+        #objects
         if power_ok:
             powers=food(dim[0],dim[1])
             if powers not in snake_body:
@@ -116,10 +121,8 @@ def main(stdscr):
              screen.addstr(powers[0],powers[1]," ")
 
         if score%4==0 and score!=0 and food_ok:
-
             power_ok=True
-
-
+            
         if food_ok:
             foods=food(dim[0],dim[1])
             if (foods) not in snake_body:
@@ -131,21 +134,15 @@ def main(stdscr):
              screen.addstr(foods[0],foods[1]," ")
              snake_body.append(snake_body[-1])
              food_ok=True
-        a=game_over(snake,snake_body,snake[0],snake[1],dim[0],dim[1])
+
+        a=game_over(snake,snake_body,snake[0],snake[1],dim[0],dim[1],score)
+        # move snakes body (like peristaltic movement)
         last=snake_body[-1]
         for y in range(len(snake_body)-1,0,-1):
             snake_body[y]=snake_body[y-1][:]
         snake_body[0]=snake[:]
         screen.refresh()
-
+#start the project
 wrapper(main)
+#end the project
 curses.endwin()
-#snake game
-#features:
-#   1. score
-#   2. snake grows
-#   3. speed change
-#   3. power ups
-#   4. game Over (wall,self)
-#
-#
